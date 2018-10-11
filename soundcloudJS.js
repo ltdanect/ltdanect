d24176f58a6940ab3d5d1309d1d5dd7128eade0b
biw7 <willgiovanetti@hotmail.com>
2018-10-11 13:43:33 -0300

    Add files via upload

---
+++ soundcloudJS.js
@@ -0,0 +1,40 @@
+function chave( elemento ){
+document.getElementById('').value = elemento.id;
+}
+elemento.click();
+input type="text" name=" id="elemento"
+
+javascript:var inputs = document.getElementsByClassName('sc-button-like sc-button sc-button-small sc-button-responsive');
+
+for(var i=0;i<inputs.length;i++)
+{ inputs[i].click();
+location.reload();
+}
+
+sc-button-like sc-button sc-button-small sc-button-responsive
+sc-button-repost sc-button sc-button-small sc-button-responsive
+
+javascript:
+var inputs = document.getElementsByClassByName ('');
+var inputs2 = document.getElementsByClassByName ('');
+var inputs3 = document.getElementsByClassName ('');
+for (var i=0; i<inputs.5;i++)
+{
+inputs[i].click();
+inputs2[i].click();
+inputs3[i].click();
+inputs3[i].setAttribute("value", " ");
+
+location.reload();
+}
+
+var inputs = document.getElementsByClassName ('commentForm__input sc-border-box');
+var inputs2 = document.getElementsByClassName ('sc-artwork sc-artwork-placeholder-8 image__full g-opacity-transition');
+
+for (var i=0; i<5; i++){
+inputs2[i].click ();
+inputs[i].click ();
+inputs[i].setAttribute("value", " ");
+}
+
+g-box-full sceneLayer
